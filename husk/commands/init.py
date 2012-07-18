c6828ac45294a57571b93d27ba01da7a3991b64a
Byron Ruth <b@devel.io>
2012-07-18 17:37:10 -0400

    Rename `init` command argument `repo` to `path` The repo is not yet created, thus it does not make sense to call the argument 'repo'.

--- husk/commands/init.py
+++ husk/commands/init.py
@@ -5,11 +5,11 @@ from husk.config import HUSK_REPO_DIR
 
 parser = ArgumentParser()
 
-parser.add_argument('repo', nargs='?', help='Path to Husk repository')
+parser.add_argument('path', nargs='?', help='Path to Husk repository')
 parser.add_argument('-a', '--add-config', action='store_true',
     help='Add a copy of the default config into the repo ' \
         '{} directory.'.format(HUSK_REPO_DIR))
 
 
 def main(options):
-    Repo.init(options.repo, options.add_config)
+    Repo.init(options.path, options.add_config)
