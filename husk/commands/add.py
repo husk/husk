acb3b67bb4b4b4de1064fea786402a643eab3061
Byron Ruth <b@devel.io>
2012-07-20 17:52:59 -0400

    Add description for CLI `add` command

--- husk/commands/add.py
+++ husk/commands/add.py
@@ -1,10 +1,15 @@
 from argparse import ArgumentParser
 from husk.repo import Repo
 
+__doc__ = """\
+Adds a bundle to the repository specified by it's `path`. If `path`
+is an existing directory/bundle, pass `--force` to register it with
+the repository. Existing files of the same name will not be overwritten.
+"""
 
-parser = ArgumentParser()
+parser = ArgumentParser(description=__doc__)
 
-parser.add_argument('path', help='Path to notes directory')
+parser.add_argument('path', help='Path to bundle directory')
 parser.add_argument('-r', '--repo', help='Path to Husk repository')
 
 
